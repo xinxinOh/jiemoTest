5cf5632f3cfd450ff6c8e102eb97e309af91af74
CangDog <37903864+CangDog@users.noreply.github.com>
2019-06-21 14:53:50 +0800

    update

--- JiemoTest/src/main/java/com/neuedu/JiemoTest/controller/QuestionBankController.java
+++ JiemoTest/src/main/java/com/neuedu/JiemoTest/controller/QuestionBankController.java
@@ -1,5 +1,7 @@
 package com.neuedu.JiemoTest.controller;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServlet;
@@ -8,7 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -35,20 +39,45 @@ public class QuestionBankController {
 	UserInfoMapper userInfoMapper;
 	
 	@Test
-	@RequestMapping("loginForTest")
+	@RequestMapping("/loginForTest")
 	public String LoginForTest(HttpServletRequest request) {
-		UserInfo user = userInfoMapper.selectByPrimaryKey(1);
+		UserInfo user = userInfoMapper.selectByPrimaryKey(2);
 		request.getSession().setAttribute("user", user);
 		return "Exam";
 	}
 	
-	@RequestMapping("showQuestionBanks")
-	public String show(HttpServletRequest request) {
+	@RequestMapping("/showQuestionBanks")
+	public String showQuestionBanks(HttpServletRequest request , Model model) {
 		UserInfo user = (UserInfo) request.getSession().getAttribute("user");
-		System.out.println("user!!!!!!!!!!!!!!!!!!!11"+user.getUserid());
+		System.out.println("user!!!!!!!!!!!!!!!!!!!"+user.getUserid());
 		//查找bank
-		List<Bank> banks= questionBankService.selectByUser(user);
+		List<Bank> banks= questionBankService.selectByUser(user,1);
+		//System.out.println(banks.size());
+		model.addAttribute("banksList", banks);
+		//System.out.println(banks.toString());
 		return "bank/banks";
+	}
+	
+	//展示垃圾桶
+	@RequestMapping("/showashBin")
+	public String showashBin(HttpServletRequest request , Model model) {
+		UserInfo user = (UserInfo) request.getSession().getAttribute("user");
+		List<Bank> banks = questionBankService.selectByUser(user, 0);
+		model.addAttribute("banksList",banks);
+		return "bank/ashBin";
+	}
+	
+	//删除题库
+	@RequestMapping("/deleteBank")
+	public String deleteBank(HttpServletRequest request , Model model) {
+		//放入回收站
+		int id = Integer.parseInt(request.getParameter("id"));
+		questionBankService.deletBank(id);
+		//更新model
+		UserInfo user = (UserInfo) request.getSession().getAttribute("user");
+		List<Bank> banks = questionBankService.selectByUser(user, 1);
+		model.addAttribute("banksList",banks);
+		return "thymeleaft:bank/banks";
 	}
 	
 }
